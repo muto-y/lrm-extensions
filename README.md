4a311d3c889ef148360684c2312db8d57716c7e3
wwwouaiebe <github.christian@guyette.be>
2016-04-28 22:20:07 +0200

    Multiple changes after tests

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # leaflet-routing-gpx
-Plugin for leaflet-routing-machine
+
+Warning : this plugin is under development. Unstable version!
+
+leaflet-routing-gpx is a plugin for leaflet-routing-machine. With this plugin, you can export to a GPS the route created with leaflet-routing-machine.
+The plugin work with leaflet-routing-machine and with lrm-mapzen (and perhaps also with others lrm plugin, but not tested... :-( ).
+See the [demo](http://wwwouaiebe.github.io/leaflet-routing-gpx/) page.
+
+On the demo, select the alternate route in blue, add waypoints or drag and drop the start or end waypoint to see the gpx file changes.
+You can also select another option on the top right.
+
+## Loading leaflet-routing-gpx
+
+You have first to have Leaflet and Leaflet-Routing-Machine loaded and eventually lrm-mapzen:
+in the header:
+
+```HTML
+		<link rel="stylesheet" href="leaflet.css" />
+		<link rel="stylesheet" href="leaflet-routing-machine.css" />
+```
+
+and in the header or the body:
+
+```HTML
+		<script src="leaflet.js"></script>
+		<script src="leaflet-routing-machine.min.js"></script>
+```
+
+After, you have to load leaflet-routing-gpx:
+```HTML
+		<script src="L.Routing.Gpx.min.js"></script>
+```
+
+## What's you have to do in the javascript?
+
+You have to replace the call to the L.Routing.control factory method by a call to the L.Routing.gpx factory method:
+
+```JavaScript
+var Routing = L.Routing.gpx( { 	waypoints: [ L.latLng ( 50.51490,5.47101 ), L.latLng ( 50.50891,5.49330 ) ] /*,and eventually others lrm options */	} ).addTo( Map );
+```
+
+All the options of the L.Routing.control method remains valid, of course. Additionally, you can add some options for the L.Routing.gpx method.
+You have also to implement something for finally saving the GPX file. Due to the fact that currently, there isn't a common way for all browsers to save 
+a file on the local drive, this part is not implemented in leaflet-routing-gpx, but you can have a look on the demo file to understand how it's made for Firefox, 
+Chrome or Opera.
+
+## leaflet-routing-gpx documentation
+
+### Additionnal options
+
+|-------------------|---------|---------------|---------------------------------------------------|
+| Option            | Type    | Default value | Description                                       |
+|-------------------|---------|---------------|---------------------------------------------------|
+| GpxXmlDeclaration | Boolean | true          | The XML declaration is added to the XML string    |
+|-------------------|---------|---------------|---------------------------------------------------|
+| GpxDate           | Number  | 2             | 0 : date is not added to the XML string           |
+|                   |         |               | 1 : the date is 1970-01-01T00:00:00.000Z          |
+|                   |         |               | 2 : the date is the current date                  |
+|-------------------|---------|---------------|---------------------------------------------------|
+| GpxWaypoints      | Boolean | true          |  Waypoints are added to the XML string            |
+|-------------------|---------|---------------|---------------------------------------------------|
+| GpxRoute          | Boolean | false         |  The route description is added to the XML string |
+|-------------------|---------|---------------|---------------------------------------------------|
+| GpxTrack          | Boolean | true          |  The track is added to the XML string             |
+|-------------------|---------|---------------|---------------------------------------------------|
+
+### getGpxString ( ) method
+
+This method get the GPX string corresponding to the current route found with leaflet-routing-machine
+
+### gpxchanged event
+
+This event is fired each time the route is modified.
--- dist/L.Routing.Gpx.js
+++ dist/L.Routing.Gpx.js
@@ -23,114 +23,29 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 	L.Routing.Gpx = L.Routing.Control.extend ( {
 		options: {
 			GpxXmlDeclaration : true,
-			GpxDate : 2, // 0 = no date, 1 = 1970-01-01T00:00:00.000Z 2 = now 
+			GpxDate : 2,
 			GpxWaypoints : true,
 			GpxRoute : false,
 			GpxTrack : true
 		},
 		_GpxRoute : null,
-
 		initialize: function ( options ) {
 			L.Util.setOptions ( this, options );
 			L.Routing.Control.prototype.initialize.call ( this, options );
+		},
+		_updateLineCallback: function( err, routes ) {
+			if ( err )
+			{
+				this._GpxRoute = null;
+				this.fire ( 'gpxchanged' );
+			}
+			L.Routing.Control.prototype._updateLineCallback.call ( this, err, routes );
 		},
 		_updateLines: function ( routes ) {
 			this._GpxRoute = routes.route;
 			L.Routing.Control.prototype._updateLines.call ( this, routes );
 			this.fire ( 'gpxchanged' );
 		},
-		/*
-		// Yes I know... I can do this (work perfect with Firefox, Chrome, Opera, or Vivaldi...) :
-		// but due to a lot of problems with IE, Edge and Safari, as usual, I prefer the below solution...
-		// This &?@$£ of IE write an HTML document when using document.implementation.createDocument...
-		getGpxDocument : function ( ) {
-			var TimeStamp;
-			switch ( this.options.GpxDate ) {
-				case 0 :
-					break;
-				case 1 :
-					TimeStamp = "1970-01-01T00:00:00.000Z";
-					break;
-				default :
-					TimeStamp = new Date ( ).toISOString ( );
-					break;
-			}
-
-			var GpxDocument = document.implementation.createDocument ( "", "", null );
-			
-			var GpxElement = GpxDocument.createElement ( "gpx" );
-			
-			GpxElement.setAttribute ( "xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance" );
-			GpxElement.setAttribute ( "xmlns:xsd", "http://www.w3.org/2001/XMLSchema" );
-			GpxElement.setAttribute ( "xsi:schemaLocation", "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" );
-			GpxElement.setAttribute ( "version", "1.1" );
-			GpxElement.setAttribute ( "creator", "Leaflet-Routing-Gpx" );
-			GpxDocument.appendChild ( GpxElement );
-				
-			if ( this._GpxRoute ) {
-				var Counter = 0;
-				if ( this._GpxRoute.waypoints && this.options.GpxWaypoints ) {
-					for ( Counter = 0; Counter < this._GpxRoute.waypoints.length; Counter ++ ) {
-						var WayPointElement = GpxDocument.createElement ( "wpt" );
-						WayPointElement.setAttribute ( "lat", this._GpxRoute.waypoints [ Counter ].latLng.lat );
-						WayPointElement.setAttribute ( "lon", this._GpxRoute.waypoints [ Counter ].latLng.lng );
-						if ( TimeStamp ) {
-							WayPointElement.setAttribute ( "time", TimeStamp );
-						}
-						WayPointElement.setAttribute ( "name", "" + Counter );
-						GpxElement.appendChild ( WayPointElement );
-					}
-				}
-
-				if ( this._GpxRoute.coordinates && 0 < this._GpxRoute.coordinates.length ) {
-					if ( this.options.GpxRoute ) {
-						var RouteElement = GpxDocument.createElement ( "rte" );
-						GpxElement.appendChild ( RouteElement );
-						for ( Counter = 0; Counter < this._GpxRoute.coordinates.length; Counter++ ) {
-							var RoutePointElement = GpxDocument.createElement ( "rtept" );
-							RoutePointElement.setAttribute ( "lat", this._GpxRoute.coordinates [ Counter ].lat );
-							RoutePointElement.setAttribute ( "lon", this._GpxRoute.coordinates [ Counter ].lng );
-							if ( TimeStamp ) {
-								RoutePointElement.setAttribute ( "time", TimeStamp );
-							}
-							RoutePointElement.setAttribute ( "name", "" + Counter );
-							RouteElement.appendChild ( RoutePointElement );
-						}
-					}
-
-					if ( this.options.GpxTrack ) {
-						var TrackElement = GpxDocument.createElement ( "trk" );
-						GpxElement.appendChild ( TrackElement );
-						
-						var SegmentElement = GpxDocument.createElement ( "trkseg" );
-						TrackElement.appendChild ( SegmentElement );
-						
-						for ( Counter = 0; Counter < this._GpxRoute.coordinates.length; Counter++ ) {
-							var TrackPointElement = GpxDocument.createElement ( "trkpt" );
-							TrackPointElement.setAttribute ( "lat", this._GpxRoute.coordinates [ Counter ].lat );
-							TrackPointElement.setAttribute ( "lon", this._GpxRoute.coordinates [ Counter ].lng );
-							if ( TimeStamp ) {
-								TrackPointElement.setAttribute ( "time", TimeStamp );
-							}
-							TrackPointElement.setAttribute ( "name", "" + Counter );
-							SegmentElement.appendChild ( TrackPointElement );
-						}
-					}
-				}
-			}	
-			
-			return GpxDocument;
-		},
-		getGpxString : function ( ) {
-			var Serializer = new XMLSerializer();
-			var GPXString = Serializer.serializeToString ( this.getGpxDocument ( ) );	
-			if ( this.options.GpxXmlDeclaration ) {
-				GPXString = "<?xml version='1.0'?>\n" + GPXString;
-			}
-
-			return GPXString;
-		},
-		*/
 		getGpxString : function ( ) {
 
 			var Tab0 = "\n";
@@ -173,20 +88,31 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 							"' />";
 					}
 				}
-				if ( this._GpxRoute.coordinates && 0 < this._GpxRoute.coordinates.length ) {
-					if ( this.options.GpxRoute ) {
+				if ( this._GpxRoute.coordinates && 0 < this._GpxRoute.coordinates.length  ) {
+					if ( this.options.GpxRoute  ) {
 						GPXString += Tab1 + "<rte>";
-						for ( Counter = 0; Counter < this._GpxRoute.coordinates.length; Counter ++ ) {
-							GPXString +=
-								Tab2 + "<rtept lat='" + 
-								this._GpxRoute.coordinates [ Counter ].lat +
-								"' lon='" +
-								this._GpxRoute.coordinates [ Counter ].lng +
-								"' " +
-								TimeStamp +
-								"name='" +
-								Counter +
-								"' />";
+						if ( this._GpxRoute.instructions && 0 < this._GpxRoute.instructions.length ) {
+							for ( Counter = 0; Counter < this._GpxRoute.instructions.length; Counter++ ) {
+								GPXString +=
+									Tab2 + "<rtept lat='" + 
+									( 
+										this._GpxRoute.coordinates [ this._GpxRoute.instructions [ Counter ].index ].lat ? 
+										this._GpxRoute.coordinates [ this._GpxRoute.instructions [ Counter ].index ].lat : 
+										this._GpxRoute.coordinates [ this._GpxRoute.instructions [ Counter ].index ][ 0 ] ) +
+									"' lon='" +
+									(
+										this._GpxRoute.coordinates [ this._GpxRoute.instructions [ Counter ].index ].lng ?
+										this._GpxRoute.coordinates [ this._GpxRoute.instructions [ Counter ].index ].lng :
+										this._GpxRoute.coordinates [ this._GpxRoute.instructions [ Counter ].index ][ 1 ] ) +
+									"' " +
+									TimeStamp +
+									"name='" +
+									Counter +
+									"' " +
+									"desc='" +
+									( this._formatter.formatInstruction ( this._GpxRoute.instructions [ Counter ] ) ).replace ( '\'', '&apos;' ).replace ('\"', '&quote;').replace ( '&', '&amp;' ).replace ( '>', '&gt;' ).replace ( '<', '&lt;')  +
+									"' />" ;
+							}
 						}
 						GPXString += Tab1 + "</rte>";
 					}
@@ -197,9 +123,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 						for ( Counter = 0; Counter < this._GpxRoute.coordinates.length; Counter ++ ) {
 							GPXString +=
 								Tab3 + "<trkpt lat='" + 
-								this._GpxRoute.coordinates [ Counter ].lat +
+								( this._GpxRoute.coordinates [ Counter ].lat ? this._GpxRoute.coordinates [ Counter ].lat : this._GpxRoute.coordinates [ Counter ][ 0 ] ) +
 								"' lon='" +
-								this._GpxRoute.coordinates [ Counter ].lng +
+								( this._GpxRoute.coordinates [ Counter ].lng ? this._GpxRoute.coordinates [ Counter ].lng : this._GpxRoute.coordinates [ Counter ][ 1 ] ) +
 								"' " +
 								TimeStamp +
 								"name='" +
